d348505289127c7145432b419f042d62069848d7
gribdesbois <gribdesbois@gmail.com>
2022-04-13 16:39:07 +0200

    hero editor

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -4,16 +4,16 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero:Hero = {
+  hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm',
   };
 
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
